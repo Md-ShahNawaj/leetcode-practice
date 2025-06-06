dbe89b373d344fa1e6680a1ce8c8c0925b28f225
Md-ShahNawaj <nawaj.msn@gmail.com>
2025-06-06 18:31:15 +0100

    don’t need

--- SQL/Easy/test.sql
+++
@@ -1,7 +0,0 @@
--- Problem: 175. Combine Two Tables
--- Link: https://leetcode.com/problems/combine-two-tables/
--- Approach: LEFT JOIN
-
-SELECT FirstName, LastName
-FROM Customers
-LEFT JOIN Orders ON Customers.Id = Orders.CustomerId;
