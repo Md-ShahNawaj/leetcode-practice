83e22309a65bf930336775f785fa01855d80b489
Md-ShahNawaj <nawaj.msn@gmail.com>
2026-04-10 22:34:42 +0100

    m

--- SQL/Easy/175.Combine_Two_Tables.sql
+++ SQL/Easy/175.Combine_Two_Tables.sql
@@ -2,11 +2,7 @@
 -- Link: https://leetcode.com/problems/combine-two-tables/
 -- Approach: LEFT JOIN Persons with Addresses to include all persons and their address info if available
 
-SELECT p.id,
-       p.firstName,
-       p.lastName,
-       a.city,
-       a.state
+SELECT p.id, p.firstName, p.lastName, a.city, a.state
 FROM Person p
 LEFT JOIN Address a
   ON p.id = a.personId;
